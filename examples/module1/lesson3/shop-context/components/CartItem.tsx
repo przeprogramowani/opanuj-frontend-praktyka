14d600a6fbb9c4f45177292de1ba2d45f585e11a
psmyrdek <przemek.smyrdek@gmail.com>
2024-02-23 07:22:05 +0100

    feat: l3

---
+++ examples/module1/lesson3/shop-context/components/CartItem.tsx
@@ -0,0 +1,65 @@
+import { useContext } from 'react';
+import { Link } from 'react-router-dom';
+
+import { IoMdAdd, IoMdClose, IoMdRemove } from 'react-icons/io';
+
+import { CartItem as CartItemType } from '../types/CartItem';
+import { CartContext } from '../contexts/CartContext';
+
+const CartItem = ({ item }: { item: CartItemType }) => {
+  const { removeFromCart, addToCart, decreaseAmount } = useContext(CartContext);
+  const { id, title, image, price, amount } = item;
+
+  return (
+    <div className="flex gap-x-4 py-2 lg:px-6 border-b border-gray-200 w-full font-light text-gray-500">
+      <div className="w-full min-h-[150px] flex items-center gap-x-4">
+        <Link to={`/product/${id}`}>
+          <img className="max-w-[80px]" src={image} alt="" />
+        </Link>
+        <div className="w-full flex flex-col">
+          <div className="flex justify-between mb-2">
+            <Link
+              to={`/product/${id}`}
+              className="text-sm uppercase font-medium max-w-[240px] text-primary hover:underline"
+            >
+              {title}
+            </Link>
+            <div
+              onClick={() => removeFromCart(id)}
+              className="text-xl cursor-pointer"
+            >
+              <IoMdClose className="text-gray-500 hover:text-red-500 transition" />
+            </div>
+          </div>
+          <div className="flex gap-x-2 h-[36px] text-sm">
+            <div className="flex flex-1 max-w-[100px] items-center h-full border text-primary font-medium">
+              <div
+                onClick={() => decreaseAmount(id)}
+                className="h-full flex-1 flex justify-center items-center cursor-pointer"
+              >
+                <IoMdRemove />
+              </div>
+              <div className="h-full flex justify-center items-center px-2">
+                {amount}
+              </div>
+              <div
+                onClick={() => addToCart(item)}
+                className="h-full flex flex-1 justify-center items-center cursor-pointer"
+              >
+                <IoMdAdd />
+              </div>
+            </div>
+            <div className="flex flex-1 justify-around items-center">
+              $ {price}
+            </div>
+            <div className="flex flex-1 justify-end items-center text-primary font-medium">{`$ ${(
+              price * amount
+            ).toFixed(2)}`}</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CartItem;
---
+++ examples/module1/lesson3/shop-rtk-start/pages/Home.tsx
@@ -0,0 +1,26 @@
+import { useContext } from 'react';
+import Product from '../components/Product';
+import { ProductContext } from '../contexts/ProductContext';
+
+const Home = () => {
+  const { products } = useContext(ProductContext);
+
+  return (
+    <div>
+      <section className="py-20">
+        <div className="container mx-auto">
+          <h1 className="text-3xl font-semibold mb-10 text-center">
+            Explore Our Products
+          </h1>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 lg:mx-8 gap-[30px] max-w-sm mx-auto md:max-w-none md:mx-0">
+            {products.map((product) => {
+              return <Product product={product} key={product.id} />;
+            })}
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Home;
