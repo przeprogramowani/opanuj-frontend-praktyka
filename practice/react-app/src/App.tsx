b1b0c217095d82d795ae200f892c2b057ba22c1b
jakub <j.pawelski.it@gmail.com>
2024-02-23 09:31:33 +0100

    Formatting code

--- practice/react-app/src/App.tsx
+++ practice/react-app/src/App.tsx
@@ -2,11 +2,7 @@ import './App.css';
 import { Calculator } from './components/molecules/Calculator';
 
 const App = () => {
-  return (
-    <div>
-      <Calculator />
-    </div>
-  );
+  return <Calculator />;
 };
 
 export default App;
