ee5695afa9bff8dd868fe8fdce7577645efc4cfe
jakub <j.pawelski.it@gmail.com>
2024-02-23 15:39:21 +0100

    Added Composition in SearchForm

--- practice/react-app/src/components/molecules/SearchForm.tsx
+++ practice/react-app/src/components/molecules/SearchForm.tsx
@@ -1,47 +1,12 @@
-import { GenderType } from '../../types/Character';
-import { SortSearchOptions } from '../../types/Sort';
-import { Input } from '../atoms/Input';
-import { Select } from '../atoms/Select';
+import { ReactNode } from 'react';
 
 type SearchFormProps = {
-  name: string;
-  setName: (name: string) => void;
-  gender: GenderType;
-  setGender: (gender: GenderType) => void;
-  sortOption: SortSearchOptions;
-  setSortOption: (sortOption: SortSearchOptions) => void;
+  children: ReactNode;
 };
 
-function SearchForm({
-  name,
-  setName,
-  gender,
-  setGender,
-  sortOption,
-  setSortOption,
-}: SearchFormProps) {
+function SearchForm({ children }: SearchFormProps) {
   return (
-    <form className="space-x-4 flex items-end justify-center">
-      <Input
-        label="Name"
-        type="text"
-        placeholder="Rick Sanchez..."
-        name={name}
-        setName={setName}
-      />
-      <Select<GenderType>
-        value={gender}
-        label="Gender"
-        setGender={setGender}
-        options={['female', 'genderless', 'male', 'unknown']}
-      />
-      <Select<SortSearchOptions>
-        value={sortOption}
-        label="Sort by"
-        setGender={setSortOption}
-        options={['created', 'name']}
-      />
-    </form>
+    <form className="space-x-4 flex items-end justify-center">{children}</form>
   );
 }
 
--- practice/react-app/src/containers/CharacterSearchContainer.tsx
+++ practice/react-app/src/containers/CharacterSearchContainer.tsx
@@ -6,6 +6,8 @@ import { Character, GenderType } from '../types/Character';
 import { useFetchData } from '../hooks/useFetchData';
 import { sortData } from '../utils/Sort';
 import { SortSearchOptions } from '../types/Sort';
+import { Input } from '../components/atoms/Input';
+import { Select } from '../components/atoms/Select';
 
 function CharacterSearchContainer() {
   const [name, setName] = useState('');
@@ -21,14 +23,27 @@ function CharacterSearchContainer() {
       <div className="pt-20" />
       <SearchTitle title="Wyszukiwarka postaci Rick and Morty" />
       <div className="pt-8" />
-      <SearchForm
-        name={name}
-        setName={setName}
-        gender={gender}
-        setGender={setGender}
-        sortOption={sortOption}
-        setSortOption={setSortOption}
-      />
+      <SearchForm>
+        <Input
+          label="Name"
+          type="text"
+          placeholder="Rick Sanchez..."
+          name={name}
+          setName={setName}
+        />
+        <Select<GenderType>
+          value={gender}
+          label="Gender"
+          setGender={setGender}
+          options={['female', 'genderless', 'male', 'unknown']}
+        />
+        <Select<SortSearchOptions>
+          value={sortOption}
+          label="Sort by"
+          setGender={setSortOption}
+          options={['created', 'name']}
+        />
+      </SearchForm>
       <div className="pt-12" />
       {sortedCharacters && <CharacterList characters={sortedCharacters} />}
       <div className="pt-16" />
