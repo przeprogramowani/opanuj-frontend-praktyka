addcaa30cc16523092d5844bbf32f55e38163f91
psmyrdek <przemek.smyrdek@gmail.com>
2024-02-23 16:02:59 +0100

    feat: example solutions - l1

---
+++ examples/module1/lesson1/_solutions/validate-it/validation/messages.ts
@@ -0,0 +1,5 @@
+// Message types as const in separate module
+
+export const MESSAGE_ERROR_INVALID_INPUT = 'Invalid - empty or non integer.';
+export const MESSAGE_ERROR_INVALID_INT = 'Invalid number.';
+export const MESSAGE_SUCCESS_VALID_INT = 'Valid';
