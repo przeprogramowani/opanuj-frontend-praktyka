e75312d5508e46309cbdf41846d1ad496eb3b857
dabgan <gabriel.danilukk@gmail.com>
2024-02-24 21:37:35 +0100

    chore: update prettier

--- examples/module1/lesson1/open-closed/UsersList.tsx
+++ examples/module1/lesson1/open-closed/UsersList.tsx
@@ -1,51 +1,43 @@
-import React, { ReactNode } from 'react';
 import { useUsers } from './hooks/useUsers';
 import { User } from './User';
 
 interface UsersListProps {
-  header: string;
-  viewFn: (user: User) => JSX.Element;
+    header: string;
+    viewFn: (user: User) => JSX.Element;
 }
 
 export const UsersList = ({ header, viewFn }: UsersListProps) => {
-  const { users, removeAllFilters, filterByFriends } = useUsers();
+    const { users, removeAllFilters, filterByFriends } = useUsers();
 
-  return (
-    <div className="bg-white p-4 rounded-lg">
-      <h1 className="text-xl font-bold mb-4">{header}</h1>
-      <div className="my-4">
-        <p>Filters</p>
-        <div className="flex flex-row space-x-2">
-          <FilterButton label={'Show all'} action={removeAllFilters} />
-          <FilterButton
-            label={'Friends > 0'}
-            action={filterByFriends.bind(this, true)}
-          />
-          <FilterButton
-            label={'Friends = 0'}
-            action={filterByFriends.bind(this, false)}
-          />
+    console.log('as');
+
+    return (
+        <div className="bg-white p-4 rounded-lg">
+            <h1 className="text-xl font-bold mb-4">{header}</h1>
+            <div className="my-4">
+                <p>Filters</p>
+                <div className="flex flex-row space-x-2">
+                    <FilterButton label={'Show all'} action={removeAllFilters} />
+                    <FilterButton label={'Friends > 0'} action={filterByFriends.bind(this, true)} />
+                    <FilterButton label={'Friends = 0'} action={filterByFriends.bind(this, false)} />
+                </div>
+            </div>
+            <div className="space-y-2">
+                {users.map((user: User) => (
+                    <div key={user.id}>{viewFn(user)}</div>
+                ))}
+            </div>
         </div>
-      </div>
-      <div className="space-y-2">
-        {users.map((user: User) => (
-          <div key={user.id}>{viewFn(user)}</div>
-        ))}
-      </div>
-    </div>
-  );
+    );
 };
 
 interface FilterButtonProps {
-  label: string;
-  action: Function;
+    label: string;
+    action: Function;
 }
 
 export const FilterButton = ({ label, action }: FilterButtonProps) => (
-  <button
-    className="border border-gray-200 px-4 py-2 hover:bg-blue-200/20 rounded-xl"
-    onClick={() => action()}
-  >
-    {label}
-  </button>
+    <button className="border border-gray-200 px-4 py-2 hover:bg-blue-200/20 rounded-xl" onClick={() => action()}>
+        {label}
+    </button>
 );
