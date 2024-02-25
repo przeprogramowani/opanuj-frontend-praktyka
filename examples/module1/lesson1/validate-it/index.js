c29722fc7dfef0b41a847f6edc9a0f763d49309a
WojciechLamperski <wolamp20@gmail.com>
2024-02-25 20:07:17 +0100

    isValid function and elements are now separate from validator function

--- examples/module1/lesson1/validate-it/index.js
+++ examples/module1/lesson1/validate-it/index.js
@@ -1,18 +1,18 @@
+const button = document.getElementById('button');
+const button2 = document.getElementById('button2');
+const result = document.getElementById('result');
+const input = document.getElementById('input');
+
+const isValid = (value) => {
+  return (
+    value &&
+    Number.isInteger(Number(value)) &&
+    Number(value) > 0 &&
+    Number(value) < 100
+  )
+}
+
 function validator() {
-  const button = document.getElementById('button');
-  const button2 = document.getElementById('button2');
-  const result = document.getElementById('result');
-  const input = document.getElementById('input');
-
-  const isValid = (value) => {
-    return (
-      value &&
-      Number.isInteger(Number(value)) &&
-      Number(value) > 0 &&
-      Number(value) < 100
-    )
-  }
-
   button.addEventListener('click', () => {
     if (isValid(input.value)) {
       result.innerHTML = 'Valid'
