41db48040a83c9dded0d90a1596f1b1bb8135caf
WojciechLamperski <wolamp20@gmail.com>
2024-02-26 00:20:49 +0100

    refactoring

--- examples/module1/lesson1/solver/App.tsx
+++ examples/module1/lesson1/solver/App.tsx
@@ -1,14 +1,10 @@
 import React, { useState } from 'react';
-import { f1, f2, f3, f4 } from './functions';
+import operations from './operations.ts'
 
 const App = () => {
-  const [numA, setNumA] = useState<number>(0);
-  const [numB, setNumB] = useState<number>(0);
-  const [numC, setNumC] = useState<number | string>(0);
-
-  const doWork = (func: (a: number, b: number) => number) => {
-    setNumC(func(numA, numB));
-  };
+  const [x, setX] = useState<number>(0);
+  const [y, setY] = useState<number>(0);
+  const [result, setResult] = useState<number | string>(0);
 
   return (
     <div>
@@ -16,43 +12,31 @@ const App = () => {
         <input
           type="number"
           className="rounded-md shadow-md p-4"
-          value={numA}
-          onChange={(e) => setNumA(parseFloat(e.target.value))}
+          value={x}
+          onChange={(e) => setX(parseFloat(e.target.value))}
         />
         <input
           type="number"
           className="rounded-md shadow-md p-4"
-          value={numB}
-          onChange={(e) => setNumB(parseFloat(e.target.value))}
+          value={y}
+          onChange={(e) => setY(parseFloat(e.target.value))}
         />
       </div>
       <div className="grid grid-cols-4 gap-x-4 my-4">
-        <button
-          className="bg-blue-200 px-2 py-4 text-lg hover:bg-blue-500 hover:text-white rounded-md"
-          onClick={() => doWork(f1)}
-        >
-          +
-        </button>
-        <button
-          className="bg-blue-200 px-2 py-4 text-lg hover:bg-blue-500 hover:text-white rounded-md"
-          onClick={() => doWork(f2)}
-        >
-          -
-        </button>
-        <button
-          className="bg-blue-200 px-2 py-4 text-lg hover:bg-blue-500 hover:text-white rounded-md"
-          onClick={() => doWork(f3)}
-        >
-          *
-        </button>
-        <button
-          className="bg-blue-200 px-2 py-4 text-lg hover:bg-blue-500 hover:text-white rounded-md"
-          onClick={() => doWork(f4)}
-        >
-          /
-        </button>
+        {operations.map(({ execution, operator })=>{
+          return (
+            <button
+              className="bg-blue-200 px-2 py-4 text-lg hover:bg-blue-500 hover:text-white rounded-md"
+              onClick={() => {
+                setResult(execution(x, y));
+              }}
+            >
+              {operator}
+            </button>
+          )
+        })}
       </div>
-      <div>Result: {numC}</div>
+      <div>Result: {result}</div>
     </div>
   );
 };
---
+++ examples/module1/lesson1/solver/operations.ts
@@ -0,0 +1,23 @@
+const operations = [
+  {
+    name: 'add',
+    execution: (a, b) => a + b,
+    operator: '+'
+  },
+  {
+    name: 'subtract',
+    execution: (a, b) => a - b,
+    operator: '-'
+  },
+  {
+    name: 'multiply',
+    execution: (a, b) => a * b,
+    operator: '*'
+  },
+  {
+    name: 'divide',
+    execution: (a, b) => a / b,
+    operator: '/'
+  }
+]
+export default operations
