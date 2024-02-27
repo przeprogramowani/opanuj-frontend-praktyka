aa5f6f6e23791b8067113adf155e95c02def35d7
MaDud <martaduda@interia.eu>
2024-02-27 20:14:07 +0100

    add signals

--- examples/module1/lesson2/signals-angular-finish/src/app/services/character-search.service.ts
+++ examples/module1/lesson2/signals-angular-finish/src/app/services/character-search.service.ts
@@ -1,79 +1,72 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable, Signal, signal } from '@angular/core';
-import { toSignal } from '@angular/core/rxjs-interop';
 import {
-  BehaviorSubject,
-  Observable,
-  combineLatest,
-  map,
-  of,
-  switchMap,
-} from 'rxjs';
+  Injectable,
+  WritableSignal,
+  computed,
+  effect,
+  signal,
+} from '@angular/core';
 import { Character } from '../types/Character';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CharacterSearchService {
-  private charactersSource$: Observable<Character[]> = new Observable<
-    Character[]
-  >();
-  characters: Signal<Character[]> = signal([]);
-  private nameSubject = new BehaviorSubject<string>('');
-  private genderSubject = new BehaviorSubject<string>('');
-  private sortOptionSubject = new BehaviorSubject<string>('');
+  private charactersSource: WritableSignal<Character[]> = signal([]);
+  private nameSubject = signal('');
+  private genderSubject = signal('');
+  private sortOptionSubject = signal('');
   private apiBaseUrl = 'https://rickandmortyapi.com/api/character/';
 
-  constructor(private http: HttpClient) {
-    this.charactersSource$ = combineLatest([
-      this.nameSubject,
-      this.genderSubject,
-    ]).pipe(
-      switchMap(([name, gender]) => {
-        if (name !== '' || gender !== '') {
-          return this.http
-            .get<{ results: Character[] }>(
-              `${this.apiBaseUrl}?name=${name}&gender=${gender}`
-            )
-            .pipe(map((response) => response.results || []));
-        } else {
-          return of([]);
-        }
-      })
-    );
+  characters = computed(() =>
+    this.sortCharacters(this.charactersSource(), this.sortOptionSubject())
+  );
 
-    this.characters = toSignal(
-      combineLatest([this.charactersSource$, this.sortOptionSubject]).pipe(
-        map(([characters, sortOption]) =>
-          this.sortCharacters(characters, sortOption)
-        )
-      ),
-      { initialValue: [] }
+  constructor(private http: HttpClient) {
+    effect(
+      (cleanUp) => {
+        if (this.nameSubject() !== '' || this.genderSubject() !== '') {
+          this.charactersSource.set([]);
+        }
+
+        const subscription = this.http
+          .get<{ results: Character[] }>(
+            `${
+              this.apiBaseUrl
+            }?name=${this.nameSubject()}&gender=${this.genderSubject()}`
+          )
+          .subscribe((response) => {
+            this.charactersSource.set(response.results);
+          });
+
+        cleanUp(() => subscription.unsubscribe);
+      },
+      { allowSignalWrites: true }
     );
   }
 
   setName(name: string) {
-    this.nameSubject.next(name);
+    this.nameSubject.set(name);
   }
 
   setGender(gender: string) {
-    this.genderSubject.next(gender);
+    this.genderSubject.set(gender);
   }
 
   setSortOption(sortOption: string) {
-    this.sortOptionSubject.next(sortOption);
+    this.sortOptionSubject.set(sortOption);
   }
 
   get name(): string {
-    return this.nameSubject.value;
+    return this.nameSubject();
   }
 
   get gender(): string {
-    return this.genderSubject.value;
+    return this.genderSubject();
   }
 
   get sortOption(): string {
-    return this.sortOptionSubject.value;
+    return this.sortOptionSubject();
   }
 
   sortCharacters(characters: Character[], sortOption: string): Character[] {
