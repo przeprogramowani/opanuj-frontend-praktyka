a70411c7fc83e664cc884759049d4a983d56d508
psmyrdek <przemek.smyrdek@gmail.com>
2024-02-29 07:57:09 +0100

    feat: add cors middleware

--- examples/scripts/vite-api.mjs
+++ examples/scripts/vite-api.mjs
@@ -1,8 +1,10 @@
+import cors from 'cors';
 import { timeoutMiddleware } from './middlewares/timeout-middleware.mjs';
 import { createDataMiddleware } from './middlewares/data-middleware.mjs';
 import { forceErrorMiddleware } from './middlewares/force-error-middleware.mjs';
 
 export const configureExpressApp = (app) => {
+  app.use(cors());
   app.use(forceErrorMiddleware);
   app.use(timeoutMiddleware);
   app.use('/api/data', createDataMiddleware());
