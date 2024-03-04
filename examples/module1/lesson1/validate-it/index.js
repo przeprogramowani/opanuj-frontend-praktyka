3e46b86e7a1347b75e769658274d6ce2a4ec1f1f
Dawid Zięba <dawid.zieba.it@gmail.com>
2024-03-04 10:05:11 +0100

    Refactoring: make isValidated more simple

--- examples/module1/lesson1/validate-it/index.js
+++ examples/module1/lesson1/validate-it/index.js
@@ -4,18 +4,15 @@ function validator() {
   const button2 = document.getElementById('button2');
   const result = document.getElementById('result');
 
-  const isValidated = (value) => {
-    value = Number(value);
-    if (!value) return false;
-    if (!Number.isInteger(value)) return false;
-    return (
-      value > 0 &&
-      value < 100 &&
-      value % 2 === 0
-    )
-  }
+  const isValidated = (value) => (
+    value &&
+    Number.isInteger(value) &&
+    value > 0 &&
+    value < 100 &&
+    value % 2 === 0
+  );
   const handleValidateIt = () => {
-      result.innerHTML = isValidated(input.value) ? 'Valid' : 'Invalid';
+      result.innerHTML = isValidated(Number(input.value)) ? 'Valid' : 'Invalid';
   }
 
   const handleClear = () => {
