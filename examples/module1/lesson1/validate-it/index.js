3f1ebb6c788f57c751f5a658b8c3ec52660da712
Dawid Zięba <dawid.zieba.it@gmail.com>
2024-03-04 10:19:12 +0100

    Refactoring: Separation of validation rules into separate functions

--- examples/module1/lesson1/validate-it/index.js
+++ examples/module1/lesson1/validate-it/index.js
@@ -4,21 +4,24 @@ function validator() {
   const button2 = document.getElementById('button2');
   const result = document.getElementById('result');
 
-  const isValidated = (value) => (
-    value &&
-    Number.isInteger(value) &&
-    value > 0 &&
-    value < 100 &&
-    value % 2 === 0
-  );
+  const isNotEmpty = (value) => !!value;
+  const greaterThen = (value, then = 0) => value > then;
+  const lessThen = (value, then = 100) => value < then;
+  const checkEven = (value) => value % 2 === 0;
+
+  const checkValidationRoles = (value, rules) => {
+    const valueAsNumber = Number(value);
+    return rules.every((rule) => rule(valueAsNumber));
+  };
   const handleValidateIt = () => {
-      result.innerHTML = isValidated(Number(input.value)) ? 'Valid' : 'Invalid';
-  }
+    const isValid = checkValidationRoles(input.value, [isNotEmpty, Number.isInteger, greaterThen, lessThen, checkEven]);
+    result.innerHTML = isValid ? 'Valid' : 'Invalid';
+  };
 
   const handleClear = () => {
     input.value = '';
     result.innerHTML = '';
-  }
+  };
 
   button.addEventListener('click', handleValidateIt);
   button2.addEventListener('click', handleClear);
