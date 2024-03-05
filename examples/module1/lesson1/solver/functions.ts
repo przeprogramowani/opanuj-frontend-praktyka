a99c50278de45805a7ea5120e75e2b5f3442e147
Dawid Zięba <dawid.zieba.it@gmail.com>
2024-03-05 06:50:25 +0100

    Refactoring: Add types to functions

--- examples/module1/lesson1/solver/functions.ts
+++ examples/module1/lesson1/solver/functions.ts
@@ -1,13 +1,15 @@
-export function add(a, b) {
+export function add(a: number, b: number): number {
   return a + b;
 }
-export function subtract(a, b) {
+
+export function subtract(a: number, b: number): number {
   return a - b;
 }
-export function divide(a, b) {
+
+export function divide(a: number, b: number): number {
   return a * b;
 }
 
-export function multiply(a, b) {
+export function multiply(a: number, b: number): number {
   return a / b;
 }
