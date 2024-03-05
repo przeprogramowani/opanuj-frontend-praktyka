891c3783192d21760b6f8b83bec95d1c327ae2a6
Dawid Zięba <dawid.zieba.it@gmail.com>
2024-03-05 07:42:54 +0100

    Add new components

--- examples/module1/lesson1/solver/App.tsx
+++ examples/module1/lesson1/solver/App.tsx
@@ -1,29 +1,31 @@
 import React, { useState } from 'react';
 import { add, subtract, divide, multiply } from './functions';
-import Input from './Input';
-import Button from './Button.tsx';
+import Input from './components/Input';
+import Button from './components/Button';
+import Operations from './components/Operations';
+import Inputs from './components/Inputs';
 
 const App = () => {
   const [param1, setParam1] = useState<number>(0);
   const [param2, setParam2] = useState<number>(0);
   const [result, setResult] = useState<number | string>(0);
 
-  const doWork = (func: (a: number, b: number) => number) => {
+  const calculate = (func: (a: number, b: number) => number) => {
     setResult(func(param1, param2));
   };
 
   return (
     <div>
-      <div className='grid grid-cols-2 gap-x-4'>
+      <Inputs>
         <Input value={param1} onChange={setParam1} />
         <Input value={param2} onChange={setParam2} />
-      </div>
-      <div className='grid grid-cols-4 gap-x-4 my-4'>
-        <Button onClick={() => doWork(add)}>+</Button>
-        <Button onClick={() => doWork(subtract)}>-</Button>
-        <Button onClick={() => doWork(divide)}>*</Button>
-        <Button onClick={() => doWork(multiply)}>/</Button>
-      </div>
+      </Inputs>
+      <Operations>
+        <Button onClick={() => calculate(add)}>+</Button>
+        <Button onClick={() => calculate(subtract)}>-</Button>
+        <Button onClick={() => calculate(divide)}>*</Button>
+        <Button onClick={() => calculate(multiply)}>/</Button>
+      </Operations>
       <div>Result: {result}</div>
     </div>
   );
---
+++ examples/module1/lesson1/solver/components/Inputs.tsx
@@ -0,0 +1,11 @@
+import React, { FC, ReactElement } from 'react';
+
+type OperationsProps = {
+  children: ReactElement[]
+};
+
+const Operations: FC<OperationsProps> = ({  children }): ReactElement => {
+  return <div className='grid grid-cols-2 gap-x-4'>{children}</div>;
+};
+
+export default Operations;
