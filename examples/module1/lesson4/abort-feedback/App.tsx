dea7207234c0c564742f5b76590344bf8a969c9a
Mateusz <mateusz.kajsztura@bannerflow.com>
2024-03-06 21:17:05 +0100

    fix timeout buttons

--- examples/module1/lesson4/abort-feedback/App.tsx
+++ examples/module1/lesson4/abort-feedback/App.tsx
@@ -11,20 +11,20 @@ const App = () => {
   return (
     <div>
       <div className="flex flex-row items-center justify-between py-4">
-        <h1 className="text-2xl font-bold">Users [timeout=4000]</h1>
+        <h1 className="text-2xl font-bold">Users [timeout=4s]</h1>
         {isTimeoutError && (
           <div className="flex flex-row items-center">
             <p className="mr-2">
               Sorry, there seems to be connectivity issues...
             </p>
             <button
-              className="text-blue-400 bg-blue-200 hover:text-blue-200 hover:bg-blue-400 rounded-md p-4"
+              className="text-blue-400 bg-blue-200 hover:text-blue-200 hover:bg-blue-400 rounded-md p-4 m-2"
               onClick={() => resetUsers(2000)}
             >
               Try again with 2s timeout
             </button>
             <button
-              className="text-blue-400 bg-blue-200 hover:text-blue-200 hover:bg-blue-400 rounded-md p-4"
+              className="text-blue-400 bg-blue-200 hover:text-blue-200 hover:bg-blue-400 rounded-md p-4 m-2"
               onClick={() => resetUsers(5000)}
             >
               Try again with 5s timeout
