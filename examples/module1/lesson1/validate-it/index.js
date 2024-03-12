0ad065263c211b121c29b5cbc8d2e1fb2db3d08c
GrzegorzNecka <strzypior@gmail.com>
2024-03-12 07:02:09 +0100

    part one of refactor

--- examples/module1/lesson1/validate-it/index.js
+++ examples/module1/lesson1/validate-it/index.js
@@ -1,31 +1,3 @@
-function validator() {
-  const input = document.getElementById('input');
-  const buttonValidate = document.getElementById('button');
-  const buttonClear = document.getElementById('button2');
-  const result = document.getElementById('result');
+import main from './index.ts';
 
-  const status = { invalid: 'Invalid', valid: 'Valid', clear: '' };
-  const setStatus = (status) => (result.innerHTML = status);
-  const isValid = (value) => value > 0 && value < 100 && value % 2 === 0;
-
-  const validate = () => {
-    const value = Number(input.value);
-
-    if (Number.isNaN(value)) {
-      setStatus(status.invalid);
-      return;
-    }
-
-    isValid(value) ? setStatus(status.valid) : setStatus(status.invalid);
-  };
-
-  const clear = () => {
-    input.value = '';
-    setStatus(status.clear);
-  };
-
-  buttonValidate.addEventListener('click', validate);
-  buttonClear.addEventListener('click', clear);
-}
-
-validator();
+main();
---
+++ examples/module1/lesson1/validate-it/validator/methods.ts
@@ -0,0 +1,15 @@
+type MethodsNumber = (value: number) => boolean;
+
+const isNumber: MethodsNumber = (value) => Number.isNaN(value);
+const isIntegerValue: MethodsNumber = (value) => Number.isInteger(value);
+const isLessThan: MethodsNumber = (value) => value < 100;
+const isMoreThan: MethodsNumber = (value) => value > 0;
+const isEven: MethodsNumber = (value) => value % 2 === 0;
+
+const VALIDATOR_NUMBER = [
+  isNumber,
+  isIntegerValue,
+  isLessThan,
+  isMoreThan,
+  isEven,
+];
