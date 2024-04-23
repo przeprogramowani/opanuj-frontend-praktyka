fb140f9380a5effe6d1b580aa395a96c28ec9c8a
Karol Wasemann <k.wasemannn@lights-on.io>
2024-04-23 09:02:43 +0200

    Add abort feedback functionality to user list

---
+++ examples/module1/lesson4/abort-feedback/useUser.ts
@@ -0,0 +1,37 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+const API_URL = '/api/data/users?timeout=3000';
+
+interface User {
+  id: number;
+  name: string;
+}
+export function useUsers() {
+  const [users, setUsers] = useState<User[]>([]);
+  const [isTimeoutError, setIsTimeoutError] = useState(false);
+
+  function fetchUsers() {
+    axios
+      .get<User[]>(API_URL, { timeout: 5000 })
+      .then((response) => {
+        setUsers(response.data);
+      })
+
+      .catch((error) => {
+        if (error.code === 'ECONNABORTED') {
+          setIsTimeoutError(true);
+        }
+      });
+  }
+
+  function retryRequest() {
+    setIsTimeoutError(false);
+    fetchUsers();
+  }
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  return { users, isTimeoutError, retryRequest };
+}
