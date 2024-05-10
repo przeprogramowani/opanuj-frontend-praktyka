f21b3661eed49edf9ab1de8cd3b48f7707df7189
GrzegorzNecka <global-strzypior@gmail.com>
2024-05-10 13:57:32 +0200

    task: axios iterceptor

--- examples/module1/lesson4/axios-interceptor/index.js
+++ examples/module1/lesson4/axios-interceptor/index.js
@@ -1,15 +1,27 @@
 import axios from 'axios';
 
-// Add a request interceptor
-axios.interceptors.request.use(function (config) {
+const requestLogger = (config) => {
+  config.metadata = {};
+  config.metadata.requestTime = Date.now();
+
   return config;
-});
+};
+
+const responseLogger = (response) => {
+  console.log(
+    `Request took ${
+      Date.now() - response.config.metadata.requestTime
+    } milliseconds.`
+  );
+
+  return response;
+};
+
+// Add a request interceptor
+axios.interceptors.request.use(requestLogger);
 
 // Add a response interceptor
-axios.interceptors.response.use(function (response) {
-  // Do something with response data
-  return response;
-});
+axios.interceptors.response.use(responseLogger);
 
 const {
   data: { articles },
