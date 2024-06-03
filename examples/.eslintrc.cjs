ef6fb7914d1e7bb2f3016e02d969ab2cfeb1f6ba
dabgan <gabriel.danilukk@gmail.com>
2024-06-03 21:18:16 +0200

    configure eslint+prettier

--- examples/.eslintrc.cjs
+++ examples/.eslintrc.cjs
@@ -6,8 +6,20 @@ module.exports = {
         project: true,
         tsconfigRootDir: __dirname,
     },
+    plugins: ['@typescript-eslint', 'prettier'],
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:playwright/recommended',
+        'prettier',
+        'prettier/@typescript-eslint',
+    ],
+    rules: {
+        '@typescript-eslint/require-await': 'error',
+        '@typescript-eslint/no-floating-promises': 'error',
+        'prettier/prettier': 'error',
+    },
     overrides: [
-        // Konfiguracja dla lekcji 2x3: Testy e2e
         {
             files: ['**/tests/**/*.{js,ts,tsx}', '**/pages/**/*.{js,ts,tsx}', '**/mocks/**/*.{js,ts,tsx}'],
             extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:playwright/recommended'],
