8653298f948377bd436de8611c84426703e81ce5
jjxmonster <kuba.tarabasz@wp.pl>
2024-06-17 23:04:27 +0200

    [M1L1] Validation Refactor

--- examples/module1/lesson1/validate-it/index.js
+++ examples/module1/lesson1/validate-it/index.js
@@ -1,34 +1,45 @@
+function getChecksValidation(value, checks) {
+  const checksResults = checks.map((check) => check(value));
+
+  if (checksResults.every((item) => item == true)) {
+    return 'Valid';
+  }
+
+  return 'Invalid';
+}
+
+const isInRange = (value) => value > 0 && value < 100;
+const isOdd = (value) => value % 2 === 0;
+
+const checksArray = [isInRange, isOdd];
+
+function handleClearInputAndResult(input, result) {
+  input.value = '';
+  result.innerHTML = '';
+}
+function handleValidateInput(input) {
+  if (!input.value) {
+    return 'Invalid';
+  } else {
+    const value = Number(input.value);
+    return getChecksValidation(value, checksArray);
+  }
+}
+
 function validator() {
   const input = document.getElementById('input');
   const button = document.getElementById('button');
   const button2 = document.getElementById('button2');
   const result = document.getElementById('result');
 
-  button.addEventListener('click', () => {
-    if (input.value) {
-      if (Number.isInteger(input.value)) {
-        if (
-          Number(input.value) > 0 &&
-          Number(input.value) < 100 &&
-          Number(input.value) % 2 === 0
-        ) {
-          result.innerHTML = 'Valid';
-        } else {
-          result.innerHTML = 'Invalid';
-        }
-        result.innerHTML = 'Valid';
-      } else {
-        result.innerHTML = 'Invalid';
-      }
-    } else {
-      result.innerHTML = 'Invalid';
-    }
-  });
+  button.addEventListener(
+    'click',
+    () => (result.innerHTML = handleValidateInput(input))
+  );
 
-  button2.addEventListener('click', () => {
-    input.value = '';
-    result.innerHTML = '';
-  });
+  button2.addEventListener('click', () =>
+    handleClearInputAndResult(input, result)
+  );
 }
 
 validator();
