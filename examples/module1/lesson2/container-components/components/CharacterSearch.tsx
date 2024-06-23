11e8430fb0e013f31e2cc471ca1a6d04e37d679f
MariuszRozycki <mariansc83@icloud.com>
2024-06-23 13:05:09 +0200

    fix: Extract character search logic to an external module

---
+++ examples/module1/lesson2/container-components/components/CharacterSearch.tsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import {Character} from "../types/Character";
+
+function CharacterSearch(name: string, gender: string){
+  const [characters, setCharacters] = useState<Character[]>([]);
+  const [sortOption, setSortOption] = useState('');
+
+  useEffect(() => {
+    if(name || gender) {
+      fetch(`https://rickandmortyapi.com/api/character/?name=${name}&gender=${gender}`)
+        .then(response => response.json())
+        .then(data => setCharacters(data.results || []))
+        .catch(error => console.error(`Error fetching data: ", ${error}`))
+    }
+  }, [name, gender]);
+
+
+  const sortedCharacters = [...characters].sort((a, b) => {
+    if (sortOption === "name") a.name.localeCompare(b.name);
+    if (sortOption === 'created') new Date(a.created).getTime() - new Date(b.created).getTime();
+
+    return 0;
+  });
+
+  return { sortedCharacters, sortOption, setSortOption };
+}
+
+export default CharacterSearch;
