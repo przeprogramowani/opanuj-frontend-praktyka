a907b4dec6f767540a4451649787f26082016371
MariuszRozycki <mariansc83@icloud.com>
2024-06-23 12:30:51 +0200

    fix: add missing React imports in various components

--- examples/module1/lesson2/container-components/containers/CharacterSearchContainer.tsx
+++ examples/module1/lesson2/container-components/containers/CharacterSearchContainer.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { useEffect, useState } from 'react';
 import CharacterList from '../components/CharacterList';
 import SearchForm from '../components/SearchForm';
