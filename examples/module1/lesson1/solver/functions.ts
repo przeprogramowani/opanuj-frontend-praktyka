261a9d381831f40aed23cbf5a8e00a6d31d59acd
rafalwojciechrolsky <raaf.em@gmail.com>
2024-06-24 15:34:24 +0200

    solver

--- examples/module1/lesson1/solver/functions.ts
+++ examples/module1/lesson1/solver/functions.ts
@@ -1,13 +1,28 @@
-export function f1(a, b) {
+export function add(a: number, b: number) {
   return a + b;
 }
-export function f2(a, b) {
+export function sub(a: number, b: number) {
   return a - b;
 }
-export function f3(a, b) {
+export function multi(a: number, b: number) {
   return a * b;
 }
 
-export function f4(a, b) {
+export function div(a: number, b: number) {
   return a / b;
 }
+
+export const calculate = (
+  setResult: (a: number) => void,
+  setError: (a: boolean) => void,
+  mathOperationFunction: (a: number, b: number) => number,
+  operandLeft: number,
+  operandRight: number
+) => {
+  if (operandRight === 0 && mathOperationFunction.name === 'div') {
+    setError(true);
+  } else {
+    setError(false);
+    setResult(mathOperationFunction(operandLeft, operandRight));
+  }
+};
