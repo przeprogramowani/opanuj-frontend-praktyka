95cc967321dbc4921fb572f8d7ed2a5b65af65c2
adamkokoszka <adam.kokoszka@antologic.com>
2024-06-24 12:01:37 +0200

    solver

--- examples/module1/lesson1/solver/functions.ts
+++ examples/module1/lesson1/solver/functions.ts
@@ -1,13 +1,14 @@
-export function f1(a, b) {
-  return a + b;
-}
-export function f2(a, b) {
-  return a - b;
-}
-export function f3(a, b) {
-  return a * b;
-}
-
-export function f4(a, b) {
-  return a / b;
+export const recalculate = (a: number, b: number, operation: string) => {
+  switch (operation) {
+    case 'addition':
+      return a + b;
+    case 'subtraction':
+      return a - b;
+    case 'multiplication':
+      return a * b;
+    case 'division':
+      return a / b;
+    default:
+      return 0
+  }
 }
