550ecb18bc5823b09024bd146999fd5d01fbb0b1
bronisMateusz <mateusz.bronis99@gmail.com>
2024-06-26 00:12:50 +0200

    refactor: Move validation messages to separate file

---
+++ examples/module1/lesson1/validate-it/validation/messages.ts
@@ -0,0 +1,3 @@
+export const MESSAGE_EMPTY_INPUT = 'Invalid. Value should not be empty';
+export const MESSAGE_INVALID_RANGE = (min: number, max: number) => `Invalid. Value should be in range ${min}-${max}`;
+export const MESSAGE_VALID = (min: number, max: number) => `Valid. Value is in range ${min}-${max}`;
