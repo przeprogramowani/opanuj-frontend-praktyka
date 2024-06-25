a37da9be188a913ef7da70f253f4c60201d607b2
bronisMateusz <mateusz.bronis99@gmail.com>
2024-06-26 00:33:26 +0200

    refactor: Move functions to separate directory

---
+++ examples/module1/lesson1/solver/calculator/functions.ts
@@ -0,0 +1,4 @@
+export const sum = (a: number, b: number) => a + b;
+export const sub = (a: number, b: number) => a - b;
+export const mul = (a: number, b: number) => a * b;
+export const div = (a: number, b: number) => a / b;
