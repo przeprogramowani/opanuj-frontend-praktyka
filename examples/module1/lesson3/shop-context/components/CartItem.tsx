cb195e6480337ffe73ad2402722885bd101bb6cd
kliwinski <kliwinski@deloittece.com>
2024-06-27 15:26:54 +0200

    Added memo to CartItem and Header

--- examples/module1/lesson3/shop-context/components/CartItem.tsx
+++ examples/module1/lesson3/shop-context/components/CartItem.tsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { memo, useContext } from 'react';
 import { Link } from 'react-router-dom';
 
 import { IoMdAdd, IoMdClose, IoMdRemove } from 'react-icons/io';
@@ -6,24 +6,36 @@ import { IoMdAdd, IoMdClose, IoMdRemove } from 'react-icons/io';
 import { CartItem as CartItemType } from '../types/CartItem';
 import { CartContext } from '../contexts/CartContext';
 
+const CartItemTitle = memo(({ id, title }: { id: number; title: string }) => {
+  return (
+    <Link
+      to={`/product/${id}`}
+      className="text-sm uppercase font-medium max-w-[240px] text-primary hover:underline"
+    >
+      {title}
+    </Link>
+  );
+});
+
+const CartItemImage = memo(({ id, image }: { id: number; image: string }) => {
+  return (
+    <Link to={`/product/${id}`}>
+      <img className="max-w-[80px]" src={image} alt="" />
+    </Link>
+  );
+});
+
 const CartItem = ({ item }: { item: CartItemType }) => {
   const { removeFromCart, addToCart, decreaseAmount } = useContext(CartContext);
   const { id, title, image, price, amount } = item;
 
   return (
     <div className="flex gap-x-4 py-2 lg:px-6 border-b border-gray-200 w-full font-light text-gray-500">
+      <CartItemImage id={id} image={image} />
       <div className="w-full min-h-[150px] flex items-center gap-x-4">
-        <Link to={`/product/${id}`}>
-          <img className="max-w-[80px]" src={image} alt="" />
-        </Link>
         <div className="w-full flex flex-col">
           <div className="flex justify-between mb-2">
-            <Link
-              to={`/product/${id}`}
-              className="text-sm uppercase font-medium max-w-[240px] text-primary hover:underline"
-            >
-              {title}
-            </Link>
+            <CartItemTitle id={id} title={title} />
             <div
               onClick={() => removeFromCart(id)}
               className="text-xl cursor-pointer"
