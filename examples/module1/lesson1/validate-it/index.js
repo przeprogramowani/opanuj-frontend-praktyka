7333a0c8885ebf832d656d37d4ca87312306c795
Grzegorz Świderski <grzegorz.swiderski@lppsa.com>
2024-06-27 19:46:36 +0200

    refactor: code refactor

--- examples/module1/lesson1/validate-it/index.js
+++ examples/module1/lesson1/validate-it/index.js
@@ -1,34 +1,36 @@
-function validator() {
-  const input = document.getElementById('input');
-  const button = document.getElementById('button');
-  const button2 = document.getElementById('button2');
+// CONSTANTS
+const VALIDATE_SUCCESS_MESSAGE = 'Valid';
+const VALIDATE_ERROR_MESSAGE = 'Invalid';
+const MIN_VAL = 0;
+const MAX_VAL = 100;
+
+// VALIDATE METHODS
+const isNumber = (num) => !!Number(num);
+const isBetween = (num) => num > MIN_VAL && num < MAX_VAL
+const isEven = (num) => num % 2 === 0;
+const validate = (num) => isNumber(num) && isBetween(num) && isEven(num);
+
+document.addEventListener('DOMContentLoaded', (event) => {
+  // HTML ELEMENTS
+  const input  = document.getElementById('input');
+  const validateButton = document.getElementById('validateButton');
+  const clearButton = document.getElementById('clearButton');
   const result = document.getElementById('result');
 
-  button.addEventListener('click', () => {
-    if (input.value) {
-      if (Number.isInteger(input.value)) {
-        if (
-          Number(input.value) > 0 &&
-          Number(input.value) < 100 &&
-          Number(input.value) % 2 === 0
-        ) {
-          result.innerHTML = 'Valid';
-        } else {
-          result.innerHTML = 'Invalid';
-        }
-        result.innerHTML = 'Valid';
-      } else {
-        result.innerHTML = 'Invalid';
-      }
-    } else {
-      result.innerHTML = 'Invalid';
+  // EVENTS
+  validateButton.addEventListener('click', () => {
+    const inputValue = input.value;
+
+    if (validate(inputValue)) {
+      result.innerHTML = VALIDATE_SUCCESS_MESSAGE;
+      return;
     }
+
+    result.innerHTML = VALIDATE_ERROR_MESSAGE;
   });
 
-  button2.addEventListener('click', () => {
+  clearButton.addEventListener('click', () => {
     input.value = '';
     result.innerHTML = '';
   });
-}
-
-validator();
+});
