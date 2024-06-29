835b5365e40fafa5fc41f5f7db380ca07dd7f281
bronisMateusz <mateusz.bronis99@gmail.com>
2024-06-29 14:51:23 +0200

    refactor: Update CharacterCard component to use destructuring for character prop

--- examples/module1/lesson2/container-components/components/CharacterCard.tsx
+++ examples/module1/lesson2/container-components/components/CharacterCard.tsx
@@ -1,9 +1,6 @@
-type CharacterCardProps = {
-  name: string;
-  image: string;
-};
+import { Character } from '../types/Character';
 
-export const CharacterCard = (character: CharacterCardProps) => (
+export const CharacterCard = ({ character }: { character: Character }) => (
   <section className="flex flex-col items-center">
     <h3>{character.name}</h3>
     <img src={character.image} alt={character.name} />
