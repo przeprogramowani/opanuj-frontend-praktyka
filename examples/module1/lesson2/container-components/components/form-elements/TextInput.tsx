ab7fda8b6db68b13bfb01d28ce23b90fe28b7d5c
bronisMateusz <mateusz.bronis99@gmail.com>
2024-06-29 14:00:50 +0200

    refactor: Convert SearchForm to use reusable form elements

---
+++ examples/module1/lesson2/container-components/components/form-elements/TextInput.tsx
@@ -0,0 +1,23 @@
+type TextInputProps = {
+  label: string;
+  value: string;
+  onChange: (value: string) => void;
+  placeholder?: string;
+};
+
+function TextInput({ label, value, onChange, placeholder }: TextInputProps) {
+  return (
+    <label className="flex flex-col">
+      {label}
+      <input
+        className="mt-1 border h-7 indent-2"
+        type="text"
+        placeholder={placeholder}
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+      />
+    </label>
+  );
+}
+
+export default TextInput;
