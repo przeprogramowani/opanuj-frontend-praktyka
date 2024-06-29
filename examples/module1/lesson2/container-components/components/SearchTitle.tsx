3256fc5204860d82dbf783a477fe98fb68a0455e
bronisMateusz <mateusz.bronis99@gmail.com>
2024-06-29 14:00:16 +0200

    feat: Update SearchTitle component to accept a title prop

--- examples/module1/lesson2/container-components/components/SearchTitle.tsx
+++ examples/module1/lesson2/container-components/components/SearchTitle.tsx
@@ -1,5 +1,5 @@
-function SearchTitle() {
-  return <h1 className="text-2xl">Wyszukiwarka postaci Rick and Morty</h1>;
-}
+type SearchTitleProps = { title: string };
+
+const SearchTitle = ({ title }: SearchTitleProps) => <h1 className="text-2xl">{title}</h1>;
 
 export default SearchTitle;
