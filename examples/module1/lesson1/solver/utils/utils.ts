993800d06ad7a9dcbec5a367809a2a359c155518
Grzegorz Świderski <grzegorz.swiderski@lppsa.com>
2024-06-30 19:45:17 +0200

    refactor: refactor of solver app code

---
+++ examples/module1/lesson1/solver/utils/utils.ts
@@ -0,0 +1,13 @@
+export function handleSum(a: number, b: number) {
+  return a + b;
+}
+export function handleSubtract(a: number, b: number) {
+  return a - b;
+}
+export function handleMultiply(a: number, b: number) {
+  return a * b;
+}
+
+export function handleDivide(a: number, b: number) {
+  return a / b;
+}
