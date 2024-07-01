82413b21b66a91c4ef36293fab22820b01741520
konradlesniak <kontakt@creativesight.pl>
2024-07-01 13:15:15 +0200

    Module 1 Lesson 4 - abort feedback

--- examples/module1/lesson4/abort-feedback/App.tsx
+++ examples/module1/lesson4/abort-feedback/App.tsx
@@ -1,40 +1,33 @@
-import React, { useEffect, useState } from 'react';
-
-interface User {
-  id: number;
-  name: string;
-}
-
-const API_URL = '/api/data/users?timeout=10000';
+import { useUsers } from './useUsers';
 
 const App = () => {
-  const [users, setUsers] = useState<User[]>([]);
-
-  useEffect(() => {
-    fetch(API_URL)
-      .then((res) => res.json())
-      .then(({ users }) => {
-        setUsers(users);
-      });
-  }, []);
+  const { users, retryRequest, timeoutError } = useUsers();
 
   return (
     <div>
-      <div className="flex flex-row items-center justify-between py-4">
-        <h1 className="text-2xl font-bold">Users</h1>
-        <div className="flex flex-row items-center">
-          <p className="mr-2">
-            Sorry, there seems to be connectivity issues...
-          </p>
-          <button className="text-blue-400 bg-blue-200 hover:text-blue-200 hover:bg-blue-400 rounded-md p-4">
-            Try again
-          </button>
+      <div className='flex flex-row items-center justify-between py-4'>
+        <h1 className='text-2xl font-bold'>Users</h1>
+        <div className='flex flex-row items-center'>
+          {timeoutError && (
+            <>
+              <p className='mr-2'>
+                Sorry, there seems to be connectivity issues...
+              </p>
+
+              <button
+                className='text-blue-400 bg-blue-200 hover:text-blue-200 hover:bg-blue-400 rounded-md p-4'
+                onClick={retryRequest}
+              >
+                Try again
+              </button>
+            </>
+          )}
         </div>
       </div>
-      <ul className="space-y-2">
+      <ul className='space-y-2'>
         {users.map((user, index) => (
           <li
-            className="bg-white p-4 rounded-md border border-gray-100"
+            className='bg-white p-4 rounded-md border border-gray-100'
             key={index}
           >
             {user.name}
---
+++ examples/module1/lesson4/abort-feedback/useUsers.ts
@@ -0,0 +1,44 @@
+
+import axios from 'axios';
+import { useEffect, useState } from 'react'
+
+interface User {
+  id: number;
+  name: string;
+}
+
+const API_URL = '/api/data/users?timeout=10000';
+
+export const  useUsers = () => {
+  const [users, setUsers] = useState<User[]>([]);
+  const [timeoutError, setTimeoutError] = useState<boolean>(false);
+
+  const getUsers = () => 
+    axios
+      .get<User[]>(API_URL, { timeout: 5000 })
+      .then((response) => {
+        const { users } = response.data;
+
+        console.log(users);
+        
+        setUsers(users);
+      })
+      .catch((error) => {
+        if (error.code === 'ECONNABORTED') {
+          setTimeoutError(true);
+        }
+      });
+  
+
+  const retryRequest = () => {
+    setTimeoutError(false);
+    getUsers();
+  }
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  
+  return { timeoutError, users, retryRequest };
+}
