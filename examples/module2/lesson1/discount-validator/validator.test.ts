be147328a8d08338343086a70086562526b38369
bronisMateusz <mateusz.bronis99@gmail.com>
2024-07-09 21:15:43 +0200

    feat: Add test returning an error if age is not a number

--- examples/module2/lesson1/discount-validator/validator.test.ts
+++ examples/module2/lesson1/discount-validator/validator.test.ts
@@ -4,12 +4,12 @@ import { formValidator } from './validator';
 describe('Form validation', () => {
     test('should return an error if first name is missing', () => {
         const errors = formValidator('', 'Doe', 30);
-        expect(errors).toContain('First name is required');
+        expect(errors).toContain('First name must have at least one character');
     });
 
     test('should return an error if last name is missing', () => {
         const errors = formValidator('John', '', 30);
-        expect(errors).toContain('Last name is required');
+        expect(errors).toContain('Last name must have at least one character');
     });
 
     test('should return an error if age is negative', () => {
@@ -20,5 +20,9 @@ describe('Form validation', () => {
     test('should return no errors if all fields are valid', () => {
         const errors = formValidator('John', 'Doe', 25);
         expect(errors).toHaveLength(0);
+    });
+
+    test('should return an error if age is not a number', () => {
+        expect(() => formValidator('John', 'Doe', NaN)).toThrow('Age must be a number');
     });
 });
