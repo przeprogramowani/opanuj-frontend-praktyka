f0289e78bc321decbe7b91160096231314276a7d
bronisMateusz <mateusz.bronis99@gmail.com>
2024-07-15 23:39:30 +0200

    feat: Create book-list app

---
+++ practice/module2/lesson1/book-list/src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import BookList from './components/BookList.vue';
+</script>
+
+<template>
+  <div id="app">
+    <h1>Book List</h1>
+    <BookList />
+  </div>
+</template>
---
+++ practice/module2/lesson1/book-list/src/components/BookList.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const newBook = ref('');
+const books = ref<string[]>([]);
+
+const addBook = () => {
+  if (newBook.value.trim() !== '') {
+    books.value.push(newBook.value.trim());
+    newBook.value = '';
+  }
+};
+
+const removeBook = (index: number) => books.value.splice(index, 1);
+</script>
+
+<template>
+  <div>
+    <input v-model="newBook" placeholder="Add a new book" />
+    <button @click="addBook">Add Book</button>
+    <ul>
+      <li v-for="(book, index) in books" :key="index">
+        {{ book }}
+        <button @click="removeBook(index)">Remove</button>
+      </li>
+    </ul>
+  </div>
+</template>
