8f4f6c82e363d7555ab0c161467dd1d448e24b59
“mkczarkowski” <marcinczarkowski@onet.eu>
2024-07-16 14:46:16 +0200

    chore: provide generic way to recognise pw tests

--- examples/vitest.config.ts
+++ examples/vitest.config.ts
@@ -1,14 +1,9 @@
 import { configDefaults, defineConfig } from 'vitest/config';
 
-const PLAYWRIGHT_TESTS = [
-  'module2/lesson2/**',
-  'module2/lesson6-ai/guess-password-finish/tests/guess-pickle-rick.spec.ts',
-];
-
-const KARMA_TESTS = ['module1/lesson2/_solutions/signals-angular/**'];
+const PLAYWRIGHT_TESTS = ['**/*.spec.ts'];
 
 export default defineConfig({
   test: {
-    exclude: [...configDefaults.exclude, ...PLAYWRIGHT_TESTS, ...KARMA_TESTS],
+    exclude: [...configDefaults.exclude, ...PLAYWRIGHT_TESTS],
   },
 });
