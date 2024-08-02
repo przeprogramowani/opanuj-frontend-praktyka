bc9969ac1ad2eb4804f44d311d00868a7d20ccd7
Michał Pasynek <terabajt@terabajtlw.pl>
2024-08-02 11:00:26 +0200

    module 1 lesson 4 axios-interceptor

--- examples/module1/lesson4/axios-interceptor/index.js
+++ examples/module1/lesson4/axios-interceptor/index.js
@@ -2,14 +2,25 @@ import axios from 'axios';
 
 // Add a request interceptor
 axios.interceptors.request.use(function (config) {
+  config.metadata = { startTime: new Date() };
   return config;
 });
 
 // Add a response interceptor
-axios.interceptors.response.use(function (response) {
-  // Do something with response data
-  return response;
-});
+axios.interceptors.response.use(
+  function (response) {
+    const endTime = new Date();
+    const duration = endTime - response.config.metadata.startTime;
+    console.log(`Czas oczekiwania na API: ${duration} ms`);
+    return response;
+  },
+  function (error) {
+    const endTime = new Date();
+    const duration = endTime - error.config.metadata.startTime;
+    console.log(`Czas oczekiwania na API: ${duration} ms`);
+    return Promise.reject(error);
+  }
+);
 
 const {
   data: { articles },
