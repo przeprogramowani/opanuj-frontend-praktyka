6c89be22133c11f8128b88ab00e7702ba37f767b
Michał Pasynek <terabajt@terabajtlw.pl>
2024-08-08 11:45:52 +0200

    module 2 lesson 1 discount-validator

--- examples/module2/lesson1/discount-validator/validator.ts
+++ examples/module2/lesson1/discount-validator/validator.ts
@@ -8,13 +8,22 @@ export function formValidator(
   if (!firstName) {
     errors.push('First name is required');
   }
+  if (firstName.length < 2) {
+    errors.push('First name must be at least 2 characters long');
+  }
 
   if (!lastName) {
     errors.push('Last name is required');
   }
+  if (lastName.length < 2) {
+    errors.push('Last name must be at least 2 characters long');
+  }
 
   if (age < 0) {
     errors.push('Age must be a positive number');
+  }
+  if (isNaN(age)) {
+    errors.push('Age must be a number');
   }
 
   return errors;
