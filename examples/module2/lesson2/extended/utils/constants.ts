df68d5970ea4c74360721d4655828b3937695e4c
bronisMateusz <mateusz.bronis99@gmail.com>
2024-08-30 15:12:27 +0200

    feat: Add constants for Community Portal page and Help Center page URLs

--- examples/module2/lesson2/extended/utils/constants.ts
+++ examples/module2/lesson2/extended/utils/constants.ts
@@ -2,4 +2,12 @@ export const URLs = {
   MAIN_PAGE: 'https://en.wikipedia.org/wiki/Main_Page',
   LOGIN_PAGE:
     'https://en.wikipedia.org/w/index.php?title=Special:UserLogin&returnto=Main+Page',
+  COMMUNITY_PORTAL_PAGE: 'https://en.wikipedia.org/wiki/Wikipedia:Community_portal',
+  HELP_CENTER_PAGE: 'https://en.wikipedia.org/wiki/Wikipedia:Help_desk',
+  PAGE_SEARCHED: 'https://en.wikipedia.org/wiki/Playwright'
+};
+
+export const TEXTs = {
+  SEARCH_TEXT: 'Watchlist',
+  SEARCH_WIKIPEDIA_TEXT: 'Playwright'
 };
