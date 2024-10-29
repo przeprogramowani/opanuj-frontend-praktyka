6624bfb37be1eaed94100aa1647a4144427184ae
Piotr Rutkowski <rutkowski.2387@gmail.com>
2024-10-29 19:30:34 +0100

    M1L4 - axios-interceptor solution

--- examples/module1/lesson4/axios-interceptor/index.js
+++ examples/module1/lesson4/axios-interceptor/index.js
@@ -1,18 +1,27 @@
 import axios from 'axios';
 
+
 // Add a request interceptor
 axios.interceptors.request.use(function (config) {
+  console.log('Request start...');
+  let seconds = 0
+  config.requestStart = setInterval(() => {
+    seconds++;
+    console.clear();
+    console.log(`Request duration: ${seconds}s`);
+  }, 1000);
+
   return config;
 });
-
+xw
 // Add a response interceptor
 axios.interceptors.response.use(function (response) {
+  clearInterval(response.config.requestStart)
+  console.log(response.config);
   // Do something with response data
   return response;
 });
 
-const {
-  data: { articles },
-} = await axios.get('/api/data/articles?timeout=3000');
+const { data: { articles }, } = await axios.get('/api/data/articles?timeout=3000');
 
 document.querySelector('#data').innerHTML = articles[0].content;
