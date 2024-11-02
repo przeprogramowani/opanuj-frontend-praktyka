34112a6db9284a43fa8999ca64984eed3c30a4f9
Piotr Rutkowski <rutkowski.2387@gmail.com>
2024-11-02 14:03:45 +0100

    change app port to 3001

--- practice/react-app/vite.config.ts
+++ practice/react-app/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: 3000,
+    port: 3001,
   },
   plugins: [react()],
 });
