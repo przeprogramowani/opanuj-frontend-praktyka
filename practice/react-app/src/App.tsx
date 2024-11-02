45e656c6228f608656b16dba2191cac4c6e97b87
Piotr Rutkowski <rutkowski.2387@gmail.com>
2024-11-02 12:03:01 +0100

    Make space for another practice solution in app

--- practice/react-app/src/App.tsx
+++ practice/react-app/src/App.tsx
@@ -1,34 +1,5 @@
 import './App.css';
-import { useQuery, gql } from '@apollo/client';
-
-const GET_LOCATIONS = gql`
-  query GetLocations {
-    locations {
-      id
-      name
-      description
-      photo
-    }
-  }
-`;
-
-function DisplayLocations() {
-  const { loading, error, data } = useQuery(GET_LOCATIONS);
-
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error : {error.message}</p>;
-
-  return data.locations.map(({ id, name, description, photo }) => (
-    <div key={id}>
-      <h3>{name}</h3>
-      <img width="400" height="250" alt="location-reference" src={`${photo}`} />
-      <br />
-      <b>About this location:</b>
-      <p>{description}</p>
-      <br />
-    </div>
-  ));
-}
+import DisplayLocations from './components/DisplayLocations';
 
 export default function App() {
   return (
---
+++ practice/react-app/src/components/DisplayLocations.tsx
@@ -0,0 +1,30 @@
+import { useQuery, gql } from '@apollo/client';
+
+const GET_LOCATIONS = gql`
+  query GetLocations {
+    locations {
+      id
+      name
+      description
+      photo
+    }
+  }
+`;
+
+export default function DisplayLocations() {
+  const { loading, error, data } = useQuery(GET_LOCATIONS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error : {error.message}</p>;
+
+  return data.locations.map(({ id, name, description, photo }) => (
+    <div key={id}>
+      <h3>{name}</h3>
+      <img width="400" height="250" alt="location-reference" src={`${photo}`} />
+      <br />
+      <b>About this location:</b>
+      <p>{description}</p>
+      <br />
+    </div>
+  ));
+}
