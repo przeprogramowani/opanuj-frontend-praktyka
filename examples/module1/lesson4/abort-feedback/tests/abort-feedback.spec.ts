ee2ffc52268945d9ef20af83b4baf6900e0f5192
“mkczarkowski” <marcinczarkowski@onet.eu>
2024-12-06 20:38:58 +0100

    fix: handle retry in test env properly

--- examples/module1/lesson4/abort-feedback/tests/abort-feedback.spec.ts
+++ examples/module1/lesson4/abort-feedback/tests/abort-feedback.spec.ts
@@ -3,6 +3,21 @@ import { expect, test } from '@playwright/test';
 
 test.describe('Abort Feedback Application', () => {
   test.beforeEach(async ({ page }) => {
+    let isFirstCall = true;
+
+    // Shorten the timeout for faster test execution
+    await page.route('**/api/data/users**', async (route) => {
+      const url = new URL(route.request().url());
+      url.searchParams.set('timeout', isFirstCall ? '5500' : '100');
+      isFirstCall = false;
+
+      await route.continue({
+        url: url.toString(),
+        headers: route.request().headers(),
+        method: route.request().method(),
+      });
+    });
+
     await page.goto('/');
   });
 
@@ -17,7 +32,7 @@ test.describe('Abort Feedback Application', () => {
   test('Displays connection problem message and retry button after 5 seconds of no response', async ({
     page,
   }) => {
-    await page.waitForTimeout(6000);
+    await page.waitForTimeout(2000);
 
     await expect(
       page.getByText('Sorry, there seems to be connectivity issues...')
@@ -27,11 +42,13 @@ test.describe('Abort Feedback Application', () => {
   });
 
   test('Retrying the request after timeout', async ({ page }) => {
-    await page.waitForTimeout(6000);
+    await page.waitForTimeout(2000);
 
     await page.getByRole('button', { name: 'Try again' }).click();
 
-    await expect(page.locator('ul > li').count()).toBeGreaterThan(10);
+    await page.waitForTimeout(500);
+
+    await expect(await page.locator('ul > li').count()).toBeGreaterThan(10);
 
     await expect(
       page.getByText('Sorry, there seems to be connectivity issues...')
