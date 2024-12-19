8cdfe0a2e1e450064b3229605777c48798614ecc
grzegorz <grzegorz.szczesniak@firma.interia.pl>
2024-12-19 20:48:40 +0100

    lesson-4-abort controller

--- examples/module1/lesson4/abort-feedback/App.tsx
+++ examples/module1/lesson4/abort-feedback/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 interface User {
   id: number;
@@ -9,30 +9,76 @@ const API_URL = '/api/data/users?timeout=10000';
 
 const App = () => {
   const [users, setUsers] = useState<User[]>([]);
+  const [error, setError] = useState<string | null>(null);
+
+  const abortControllerRef = useRef<AbortController>(null);
+
+  const fetchWithTimeout = (url: string, timeout: number, signal: any) => {
+    return Promise.race([
+      fetch(url, { signal }),
+      new Promise((_, reject) =>
+        setTimeout(() => reject(new Error('Timeout')), timeout)
+      ),
+    ])
+  };
+
+  const getUsersRequest = () => {
+    setError(null);
+
+    if (abortControllerRef.current) {
+      abortControllerRef.current?.abort();
+    }
+
+    const newAbortController = new AbortController();
+
+    abortControllerRef.current = newAbortController;
+
+    const timeout = setTimeout(() => {
+      setError('Sorry, there seems to be connectivity issues...');
+    }, 5000)
+
+    fetchWithTimeout(API_URL, 11000, newAbortController.signal)
+      .then((res) => res.json())
+      .then((users) => {
+        setUsers(users);
+        setError(null);
+      }).catch((error) => {
+        if (error.message === 'Timeout') {
+          setError('Sorry, there seems to be connectivity issues...');
+        }
+      }).finally(() => {
+        clearTimeout(timeout);
+      });
+  };
 
   useEffect(() => {
-    fetch(API_URL)
-      .then((res) => res.json())
-      .then(({ users }) => {
-        setUsers(users);
-      });
+    getUsersRequest();
+    return () => {
+      // Cleanup function to abort any pending requests
+      if (abortControllerRef.current) {
+        abortControllerRef.current.abort();
+      }
+    };
   }, []);
+  
 
   return (
     <div>
       <div className="flex flex-row items-center justify-between py-4">
         <h1 className="text-2xl font-bold">Users</h1>
         <div className="flex flex-row items-center">
-          <p className="mr-2">
-            Sorry, there seems to be connectivity issues...
-          </p>
-          <button className="text-blue-400 bg-blue-200 hover:text-blue-200 hover:bg-blue-400 rounded-md p-4">
-            Try again
-          </button>
+          {error && <>
+            <p className="mr-2">
+              {error}
+            </p>
+            <button onClick={getUsersRequest} className="text-blue-400 bg-blue-200 hover:text-blue-200 hover:bg-blue-400 rounded-md p-4">
+              Try again
+            </button>
+          </>}
         </div>
       </div>
       <ul className="space-y-2">
-        {users.map((user, index) => (
+        {users?.map((user, index) => (
           <li
             className="bg-white p-4 rounded-md border border-gray-100"
             key={index}
